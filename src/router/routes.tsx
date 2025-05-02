a4f8941433581680c20e9e5925bbe2acb8485a4f
PrajwalaNagekar <prajwalanagekar@gmail.com>
2025-05-02 11:40:02 +0530

    initial commit from frontend

---
+++ src/router/routes.tsx
@@ -0,0 +1,166 @@
+import { lazy } from 'react';
+import AdminDashboard from '../pages/Admin/Dashboard';
+import Merchants from '../pages/Admin/merchant/Merchants';
+import MerchantApproval from '../pages/Admin/merchant/MerchantApproval';
+import Orders from '../pages/Admin/Orders';
+import Reports from '../pages/Admin/Reports';
+import Settings from '../pages/Admin/Settings';
+import CreateMerchant from '../components/Admin/Merchant/Create';
+import EditMerchant from '../components/Admin/Merchant/Edit';
+import MerchantData from '../pages/Admin/merchant/MerchantData';
+import AdminProtected from '../components/Protected/AdminProtected';
+import Login from '../pages/Login';
+const KnowledgeBase = lazy(() => import('../pages/Pages/KnowledgeBase'));
+const ContactUsBoxed = lazy(() => import('../pages/Pages/ContactUsBoxed'));
+const ContactUsCover = lazy(() => import('../pages/Pages/ContactUsCover'));
+const Faq = lazy(() => import('../pages/Pages/Faq'));
+const ComingSoonBoxed = lazy(() => import('../pages/Pages/ComingSoonBoxed'));
+const ComingSoonCover = lazy(() => import('../pages/Pages/ComingSoonCover'));
+const ERROR404 = lazy(() => import('../pages/Pages/Error404'));
+const ERROR500 = lazy(() => import('../pages/Pages/Error500'));
+const ERROR503 = lazy(() => import('../pages/Pages/Error503'));
+const Maintenence = lazy(() => import('../pages/Pages/Maintenence'));
+const About = lazy(() => import('../pages/About'));
+const Error = lazy(() => import('../components/Error'));
+
+const routes = [
+    // dashboard
+    {
+        path: '/',
+        element: (
+            <AdminProtected>
+                <AdminDashboard />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/merchants',
+        element: (
+            <AdminProtected>
+                <Merchants />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/merchants/:id',
+        element: (
+            <AdminProtected>
+                <MerchantData />
+            </AdminProtected>
+        )
+    },
+    {
+        path: '/admin/merchants/create',
+        element: (
+            <AdminProtected>
+                <CreateMerchant />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/merchants/edit',
+        element: (
+            <AdminProtected>
+                <EditMerchant />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/merchant-approval',
+        element: (
+            <AdminProtected>
+                <MerchantApproval />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/orders',
+        element: (
+            <AdminProtected>
+                <Orders />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/reports',
+        element: (
+            <AdminProtected>
+                <Reports />
+            </AdminProtected>
+        ),
+    },
+    {
+        path: '/admin/settings',
+        element: (
+            <AdminProtected>
+                <Settings />
+            </AdminProtected>
+        ),
+    },
+    // pages
+    {
+        path: '/pages/knowledge-base',
+        element: <KnowledgeBase />,
+    },
+    {
+        path: '/pages/contact-us-boxed',
+        element: <ContactUsBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/contact-us-cover',
+        element: <ContactUsCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/faq',
+        element: <Faq />,
+    },
+    {
+        path: '/pages/coming-soon-boxed',
+        element: <ComingSoonBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/coming-soon-cover',
+        element: <ComingSoonCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/error404',
+        element: <ERROR404 />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/error500',
+        element: <ERROR500 />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/error503',
+        element: <ERROR503 />,
+        layout: 'blank',
+    },
+    {
+        path: '/pages/maintenence',
+        element: <Maintenence />,
+        layout: 'blank',
+    },
+    {
+        path: '/login',
+        element: <Login />,
+        layout: 'blank',
+    },
+    {
+        path: '/about',
+        element: <About />,
+        layout: 'blank',
+    },
+    {
+        path: '*',
+        element: <Error />,
+        layout: 'blank',
+    },
+];
+
+export { routes };
